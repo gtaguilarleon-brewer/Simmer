4f47933e6b398c244bc4142d8c3c7726703a91f4
Graciela Brewer <gtaguilarleon@gmail.com>
2026-03-29 16:56:34 -0400

    Use publishable API key for Supabase

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://uehpssdmqwdoalrlgznz.supabase.co";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVlaHBzc2RtcXdkb2Fscmxnem56Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzOTM2NDUsImV4cCI6MjA1ODk2OTY0NX0.GiiJHMIuRRIwAbEwxW-wjJNwFaj-afGJoUbmi3PYih0";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "sb_publishable_QvEZd0guWrv6kX9iIKipcw_b05UY501";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
